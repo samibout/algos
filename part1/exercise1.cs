4cb825105889d49ad90438a14ff08c1fb8561e77
Sammien <sammi.sjodahl@passagen.se>
2020-05-07 11:20:32 +0300

    ex1done

--- part1/exercise1.cs
+++ part1/exercise1.cs
@@ -2,9 +2,16 @@ namespace part1
 {
   public class Numbers
   {
+    
     public int Sum(int x)
     {
-      return 0;
+      int sum = 0;
+      while(x>0)
+      {
+        sum += x%10;
+        x /= 10;
+      }
+      return sum;
     }
   }
 }
