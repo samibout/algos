e9b74179c613752d84e0d6b9e49e6db0a9cee0c8
Sammien <sammi.sjodahl@passagen.se>
2020-05-08 10:03:48 +0300

    ex2done

--- part1/exercise2.cs
+++ part1/exercise2.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace part1
 {
   public class Substrings
   {
     public int Calculate(string a, string b)
     {
-      return 0;
+      //first done it by myself
+      int match = 0;
+      int st = a.Length;
+      int sub = b.Length;
+
+      for(int i =0;i<st-(sub-1);i++)
+      {
+        if(a.Substring(i, sub) == b)
+        {
+            match++;
+            
+        }
+      }
+      //then found a line from internet
+      match = Regex.Matches(a, b).Count;
+      return match;
     }
   }
 }
