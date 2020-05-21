d4c6b0790ac02e870d456b37b0bd516b8489690f
Sammien <sammi.sjodahl@passagen.se>
2020-05-21 17:35:33 +0300

    ex1done

---
+++ part3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Hello(5);
+        }  
+
+        static void Hello(int n)
+        {
+          Console.WriteLine(" Hello!");
+      
+          if (n == 1) return;
+          Hello(n - 1);
+
+        }
+
+        
+    }
+}
